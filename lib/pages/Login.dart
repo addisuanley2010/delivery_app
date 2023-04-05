24a3d0cdf212b002c59cd2d3a53981e5b41e4d71
addisuanley2010 <addisuanley2005@gmail.com>
2023-04-06 01:57:53 +0300

    login Page is Updated!

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,16 +1,143 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
+  // ignore: library_private_types_in_public_api
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginScreenState extends State<LoginScreen> {
+  final _key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Account"),
+        backgroundColor: Colors.deepPurple[600],
+      ),
+      body: Container(
+        color: Colors.deepPurple,
+        child: Form(
+          key: _key,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "Please enter The Email And Password ",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(color: Colors.white)),
+                      style: const TextStyle(
+                          color: Color.fromARGB(255, 233, 10, 10)),
+                    ),
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(color: Colors.white)),
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 227, 11, 11),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    ElevatedButton(
+                      child: const Text('   Sign In  '),
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextButton(
+                          child: const Text(
+                            'Forget Password ?',
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    const Text(
+                      "or",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.facebook),
+                      label: Text("Login With Facebook"),
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.email_rounded),
+                      label: Text("Login With Google"),
+                    ),
+                  ],
+                ),
+              ),
+             const Text(
+                "Have no Account  ?",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              Padding(
+                padding:const EdgeInsets.all(20),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Create Account",
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
